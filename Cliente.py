0c5a504e9020f3d144d2c4e012e885764cba212a
hecgonzal3z <hecgonzalezsierra@ciencias.unam.mx>
2025-02-18 19:26:24 -0600

    Add files via upload

---
+++ Cliente.py
@@ -0,0 +1,15 @@
+'''Fecha: 18/02/2025
+@version: 1.0
+@author: Héctor González
+'''
+
+def__init(self,nombre,edad,direccion):
+    self.nombre=nombre
+    self.edad=edad
+    self.direccion=direccion
+    
+def imprimirDetalles(self):
+    print("Desde el método")
+    print("nombre::",self.nombre)
+    print("edad::",self.edad)
+    print("direccion::", self,direccion)
