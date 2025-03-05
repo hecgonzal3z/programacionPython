f1ac18407febf529821a7a0db1bedc4414e36882
hecgonzal3z <hecgonzalezsierra@ciencias.unam.mx>
2025-03-05 12:19:42 -0600

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,23 @@
+'''Fecha: 18/02/2025
+@version: 1.0
+@author: Héctor González
+'''
+
+from Cliente import *
+from Cuenta import *
+
+class Main:
+    pass
+
+class mensajeBienvenida:
+    def __init__():
+        pass
+    def darBienvenida():
+        print("Bienvenido")
+
+mensajeBienvenida.darBienvenida()
+
+Héctor = Cliente("Héctor,"C. San Julio no. 205", "18")
+imprimirDetalles(Juan)
+
+cuenta1 = Cuenta(5000,"debito","Héctor")
